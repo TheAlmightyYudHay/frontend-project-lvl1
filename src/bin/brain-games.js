46527b45726a19377556eaf43fb25d7ad883230b
Anton Kulish <antonsergeevichkulish@gmail.com>
2019-09-19 08:03:33 +0300

    Added readme, eslint, code climat

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import {askName} from "../index";
+import askName from '../index';
 
-console.log(`Welcome to the Brain Games!\n`);
+console.log('Welcome to the Brain Games!\n');
 
 askName();
--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import readlineSync from 'readline-sync';
 
 const askName = () => {
   const userName = readlineSync.question('May I have your name?', {
-    defaultInput: 'Mr. Incognito'
+    defaultInput: 'Mr. Incognito',
   });
   console.log(`Hello, ${userName}!`);
 };
 
-export { askName };
+export default askName;
