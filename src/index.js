cd1a479120f42dbe91e28ad46d07fbf6423feece
Anton Kulish <antonsergeevichkulish@gmail.com>
2019-09-19 05:59:04 +0300

    Added name asking functionality to the program

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import readlineSync from 'readline-sync';
+
+const askName = () => {
+  const userName = readlineSync.question('May I have your name?', {
+    defaultInput: 'Mr. Incognito'
+  });
+  console.log(`Hello, ${userName}!`);
+};
+
+export { askName };
