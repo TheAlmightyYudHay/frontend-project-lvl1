460ddf505878b871e15659653446fe25fe70e66e
Anton Kulish <antonsergeevichkulish@gmail.com>
2019-09-20 10:53:11 +0300

    Added brains-even game functionality

---
+++ src/bin/brain-even.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import { askName, greeting, askEven } from '../index';
+
+console.log('Welcome to the Brain Games!\n');
+
+const playerName = askName();
+
+greeting(playerName);
+
+const gameResult = askEven(0);
+
+if (gameResult) console.log(`Congratulations, ${playerName}!`);
--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
-import askName from '../index';
+import { askName, greeting } from '../index';
 
 console.log('Welcome to the Brain Games!\n');
 
-askName();
+const playerName = askName();
+
+greeting(playerName);
