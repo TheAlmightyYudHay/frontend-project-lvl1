3c1cc514d0a4517fb6f1625fc61f166960ed7fc4
Anton Kulish <antonsergeevichkulish@gmail.com>
2019-10-01 04:55:39 +0300

    Added description to the solution

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -22,6 +22,9 @@ const resolveProgression = (firstValue, progressionStep, positionOfHidden) => {
 };
 
 const generateRound = () => {
+  // We can set progression only with 3 values: start, step and position of hidden
+  // I understand, that I can use Array and make something like this: arr[hidden] = '..'
+  // But I do not want to use arrays in this project as much as possible
   const firstValue = getRandomNumber(1, 7);
   const progressionStep = getRandomNumber(1, 7);
   const positionOfHidden = getRandomNumber(1, progressionLengthCount);
